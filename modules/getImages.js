cde4e54e852fe186e27f160dd5e6fe32e607464f
Timothy Parker <tlparker71@gmail.com>
2022-11-11 04:18:05 +0000

    modularized getImage

--- modules/getImages.js
+++ modules/getImages.js
@@ -1,16 +1,35 @@
-const Image = require('../models/Image.js');
+const axios = require('axios');
 
-async function getImages(request, response, next){
-  try{
-  let imageUrl= `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_API_KEY}&count=20&orientation=landscape&fit=crop&w=1080&q=80&fit=max`
-  let imageData= await axios.get(imageUrl);
-  // let groomedData = imageData.data
-  let groomedData = imageData.data.filter((image)=> image.description !== null).map(image => new Image(image));
+async function getImages(request, response, error) {
+  let imageUrl = `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_API_KEY}&count=2&orientation=landscape&fit=crop&w=1080&q=80&fit=max`
+  let imageData = await axios.get(imageUrl);
+  let groomedData = imageData.data.filter((image) => image.description !== null).map(image =>
+    new Image(image)
+  );
+  groomedData.forEach(imageObj => imageObj.getWords())
   console.log(groomedData);
   response.status(200).send(groomedData);
-} catch (error) {
-  next(error);
-}
 };
+
+class Image {
+  constructor(imageObj) {
+    this.id = imageObj.id;
+    this.description = imageObj.description;
+    this.url = imageObj.urls.regular;
+    this.user = imageObj.user.name;
+    this.userPortfolio = imageObj.user.portfolio_url;
+    this.words = []
+  }
+  getWords (){
+    let descSplit = this.description.split(' ');
+    console.log(descSplit);
+    for (let i=0; i<descSplit.length;i++){
+      if(descSplit[i].length>3){
+        this.words.push(descSplit[i])
+      }
+    }
+    console.log(this.words);
+  }
+}
 
 module.exports = getImages;
--- server.js
+++ server.js
@@ -4,13 +4,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const axios = require('axios');
+
 const Music = require('./models/Music.js');
 
 const getMusic = require('./modules/getMusic.js');
 const postMusic = require('./modules/postMusic.js');
 const deleteMusic = require('./modules/deleteMusic.js');
 const updateMusic = require('./modules/updateMusic.js');
+const getImages = require('./modules/getImages.js');
 
 const app = express();
 app.use(cors());
@@ -42,39 +43,6 @@ app.delete('/music/:id', deleteMusic);
 app.put('/music/:id', updateMusic);
 
 app.get('/images', getImages);
-
-
-async function getImages(request, response, error) {
-  let imageUrl = `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_API_KEY}&count=2&orientation=landscape&fit=crop&w=1080&q=80&fit=max`
-  let imageData = await axios.get(imageUrl);
-  let groomedData = imageData.data.filter((image) => image.description !== null).map(image =>
-    new Image(image)
-  );
-  groomedData.forEach(imageObj => imageObj.getWords())
-  console.log(groomedData);
-  response.status(200).send(groomedData);
-};
-
-class Image {
-  constructor(imageObj) {
-    this.id = imageObj.id;
-    this.description = imageObj.description;
-    this.url = imageObj.urls.regular;
-    this.user = imageObj.user.name;
-    this.userPortfolio = imageObj.user.portfolio_url;
-    this.words = []
-  }
-  getWords (){
-    let descSplit = this.description.split(' ');
-    console.log(descSplit);
-    for (let i=0; i<descSplit.length;i++){
-      if(descSplit[i].length>3){
-        this.words.push(descSplit[i])
-      }
-    }
-    console.log(this.words);
-  }
-}
 
 app.get('*', (request, response) => {
   response.status(404).send(`These aren't the droids you're looking for.`)
